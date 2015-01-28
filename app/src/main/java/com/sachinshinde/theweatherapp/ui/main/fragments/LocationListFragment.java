687561f33f85f7c2f9868462854c731f1cf4b2fa
sachin1092 <me@sachinshinde.com>
2015-01-28 11:02:53 +0530

    added choice mode

--- app/src/main/java/com/sachinshinde/theweatherapp/ui/main/fragments/LocationListFragment.java
+++ app/src/main/java/com/sachinshinde/theweatherapp/ui/main/fragments/LocationListFragment.java
@@ -544,23 +544,23 @@ public class LocationListFragment extends Fragment {
 //	 * Turns on activate-on-click mode. When this mode is on, list items will be
 //	 * given the 'activated' state when touched.
 //	 */
-//	public void setActivateOnItemClick(boolean activateOnItemClick) {
-//		// When setting CHOICE_MODE_SINGLE, ListView will automatically
-//		// give items the 'activated' state when touched.
-////		mRecyclerView.setChoiceMode(
-////				activateOnItemClick ? ListView.CHOICE_MODE_SINGLE
-////						: ListView.CHOICE_MODE_NONE);
-//	}
+	public void setActivateOnItemClick(boolean activateOnItemClick) {
+		// When setting CHOICE_MODE_SINGLE, ListView will automatically
+		// give items the 'activated' state when touched.
+		mRecyclerView.setChoiceMode(
+				activateOnItemClick ? ListView.CHOICE_MODE_SINGLE
+						: ListView.CHOICE_MODE_NONE);
+	}
 //
-//	private void setActivatedPosition(int position) {
-////		if (position == ListView.INVALID_POSITION) {
-////			getListView().setItemChecked(mActivatedPosition, false);
-////		} else {
-////			getListView().setItemChecked(position, true);
-////		}
-//
-//		mActivatedPosition = position;
-//	}
+	private void setActivatedPosition(int position) {
+//		if (position == ListView.INVALID_POSITION) {
+//			getListView().setItemChecked(mActivatedPosition, false);
+//		} else {
+//			getListView().setItemChecked(position, true);
+//		}
+
+		mActivatedPosition = position;
+	}
 
     public interface OnItemClickListener {
         public void onItemClick(View view, int position);
